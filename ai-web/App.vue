8fa3c8e39644ec137d7868c07f537c55d2785052
lvnianci <lvnianci@qq.com>
2023-02-20 11:29:31 +0800

    测试

--- ai-web/App.vue
+++ ai-web/App.vue
@@ -12,6 +12,8 @@
 	}
 </script>
 
-<style>
+<style  lang="scss">
 	/*每个页面公共css */
+	//全局
+	@import "@/static/css/index.scss";
 </style>
--- ai-web/pages/index/index.vue
+++ ai-web/pages/index/index.vue
@@ -1,49 +1,19 @@
 <template>
-	<view>
-		sss
+	<view class="ai-color-primary">
+		{{a}}
 	</view>
 </template>
 
-<script>
-	export default {
-		data() {
-			return {
-				title: 'Hello'
-			}
-		},
-		onLoad() {
-
-		},
-		methods: {
-
-		}
-	}
+<script lang="ts" setup>
+	import {ref} from "vue";
+	import { onLoad } from '@dcloudio/uni-app'  
+	const a:string = ref("aabb");
+	onLoad((e) => {
+		console.log('页面加载完成',e);
+	})
+	
 </script>
 
-<style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
-
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
-	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
-	}
+<style lang="scss" scoped>
+	
 </style>
